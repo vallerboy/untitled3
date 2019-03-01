6bcc0926b1d916e9b8a3a53be7457976f3bcd498
Oskix <oskar@boxly.pl>
2019-03-01 09:31:29 +0100

    asd

---
+++ src/game/Monster.java
@@ -0,0 +1,66 @@
+package game;
+
+import java.util.Random;
+
+public class Monster {
+    private int attack;
+    private int def;
+    private int chanceToAttack;
+    private String name;
+    private static Random random = new Random();
+
+
+    public Monster(int attack, int def, int chanceToAttack, String name) {
+        this.attack = attack;
+        this.def = def;
+        this.chanceToAttack = chanceToAttack;
+        this.name = name;
+    }
+
+    /**
+     * Description :) this method is really cool !
+     * @param player Player.class
+     * @return return true if player was attacked, otherwise return false
+     */
+
+    public boolean attackPlayer(Player player){
+        if(chanceToAttack >= random.nextInt(100)){
+            return false;
+        }
+
+        player.setDef(player.getDef() - this.getAttack());
+        return true;
+    }
+
+    public int getAttack() {
+        return attack;
+    }
+
+    public void setAttack(int attack) {
+        this.attack = attack;
+    }
+
+    public int getDef() {
+        return def;
+    }
+
+    public void setDef(int def) {
+        this.def = def;
+    }
+
+    public int getChanceToAttack() {
+        return chanceToAttack;
+    }
+
+    public void setChanceToAttack(int chanceToAttack) {
+        this.chanceToAttack = chanceToAttack;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ src/game/Player.java
@@ -0,0 +1,67 @@
+package game;
+
+import java.util.Random;
+
+public class Player {
+    private int attack;
+    private int def;
+    private int chanceToCriticalAttack;
+    private String name;
+    private static Random random = new Random();
+
+    public Player(int attack, int def, int chanceToCriticalAttack, String name) {
+        this.attack = attack;
+        this.def = def;
+        this.chanceToCriticalAttack = chanceToCriticalAttack;
+        this.name = name;
+    }
+
+    public boolean attackMonster(Monster monster){
+        if(chanceToCriticalAttack >= random.nextInt(100)){
+            return false;
+        }
+
+        monster.setDef(monster.getDef() - this.getAttack());
+        return true;
+    }
+
+    public int getAttack() {
+        return attack;
+    }
+
+    public void setAttack(int attack) {
+        this.attack = attack;
+    }
+
+    public int getDef() {
+        return def;
+    }
+
+    public void setDef(int def) {
+        this.def = def;
+    }
+
+    public int getChanceToCriticalAttack() {
+        return chanceToCriticalAttack;
+    }
+
+    public void setChanceToCriticalAttack(int chanceToCriticalAttack) {
+        this.chanceToCriticalAttack = chanceToCriticalAttack;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public static Random getRandom() {
+        return random;
+    }
+
+    public static void setRandom(Random random) {
+        Player.random = random;
+    }
+}
